2e24b263bb45d041d89bce2dbefb321b66cfd0d4
guacajoely <guacajoely@gmail.com>
2023-03-08 13:54:10 -0500

    add wireframe, ERD, and skeleton

---
+++ scripts/createHTML.js
@@ -0,0 +1,22 @@
+export const createHTML = () => {
+    return `
+
+        <header>
+            <img
+        
+        </header>
+
+        <section class="areas container">
+
+        areas go here
+
+        </section>
+
+        <article class="parkguests container">
+        park guests go here
+        </article>
+
+        <footer>footer go here</footer>
+        
+      `;
+  };
